ffd3350af3eb881a7e0e6be8d7a3093f0c3b65e0
heimusu <heimusu@gmail.com>
2014-04-30 09:43:27 +0900

    modified

--- double.py
+++ double.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import cmath
 
 # 定数
 hbar = 1.054571628e-34
@@ -11,7 +12,7 @@ I = 0 + 1j
 
 # 舞台設定など
 delta_x = 10.0
-x_0 = -5 * delta_x
+x_0 = -5.0 * delta_x
 E_0 = 0.155
 S = 4500.0
 L = 14.0
@@ -155,7 +156,7 @@ if __name__ == "__main__":
     end_k = 0.8
     hx = 0.00625
     hk = 0.000125 #default:0.0005
-    double x = -1000
+    x = -1000.0
     res = 0 + 0j
     ex = int((end_x- x) / hx)
     ek = int((end_k - k1) / hk)
@@ -174,10 +175,11 @@ if __name__ == "__main__":
     N4 = int(d / hx)
     N5 = int((end_x - (L + d)) / hx)
 
-    tmp1 = abs(f(k1) * (cmath.exp(I * k1 * x) + R1(k1) * cmath.exp(-I * k1 * x)) * cmath.exp(-I * k1 * k1* s))**2
+    tmp1 = abs(f(k1) * (cmath.exp(I * k1 * x) + R1(k1) * cmath.exp(-I * k1 * x)) * cmath.exp(-I * k1 * k1 * s))**2
 
+    #入反射
     #for(nx = 0; nx <= N1; nx++){
-    for nx in range(0,N1):
+    for nx in range(0,N1+1):
 	k = k1 + hk
 	res = 0 + 0j
 	#for(jk = 1; jk < ek-1; jk+=2){
@@ -202,13 +204,162 @@ if __name__ == "__main__":
 
 	x += hx
 	#台形則
-	k_sum += (tmp1 + abs(res)**2 * hx / 2.0
+	k_sum += ((tmp1 + (abs(res)**2) * hx) / 2.0)
 	tmp1 = abs(res)**2
 
-	#cout << "x " << x  << endl;
-	#cout << "area " << k_sum << endl;
-	print(x,str(x))
-	print("area",str(k_sum))
+    #cout << "x " << x  << endl;
+    #cout << "area " << k_sum << endl;
+    print(x,str(x))
+    print("area",str(k_sum))
 
+    k_2 = k2(k1)
+    tmp1 = abs((f(k1) * ( A1(k1) * cmath.exp(I * k_2 * x) +  B1(k1) * cmath.exp(-I * k_2 * x) ) * cmath.exp(-I * k1 * k1* s)))**2
+
+    #障壁
+    #for(nx = 0; nx <= N2; nx++){
+    for nx in range(0,N2+1):
+	k = k1 + hk
+	res = 0+0j
+	k_2 = k2(k)
+	#for(jk = 1; jk<ek-1; jk+=2){
+	for jk in range(1,ek-1,2):
+	    res += 4.0 * f(k) * ( A1(k) * cmath.exp(I * k_2 * x) + B1(k) * cmath.exp(-I * k_2 * x) )* cmath.exp(-I * k * k * s)
+	    k += hk * 2
+	    k_2 = k2(k)
+
+	k = k1 + hk + hk
+	k_2 = k2(k)
+	    #for(jk = 2; jk < ek-1; jk+=2){
+	for jk in range(2,ek-1,2):
+	    res += 2.0 * f(k) * ( A1(k) * cmath.exp(I * k_2 * x) + B1(k) * cmath.exp(-I * k_2 * x) ) * cmath.exp(-I * k * k * s)
+	    k += hk * 2.0
+	    k_2 = k2(k)
+	
+	k_2 = k2(k1)
+	res += f(k1) * ( A1(k1) * cmath.exp(I * k_2 * x) + B1(k1) * cmath.exp(-I * k_2 * x) ) * cmath.exp(-I * k1 * k1* s)
+	k_2 = k2(end_k)
+	res += f(end_k) * ( A1(end_k) * cmath.exp(I * k_2 * x) + B1(end_k) * cmath.exp(-I * k_2 * x) ) * cmath.exp(-I * end_k * end_k * s)
+	res  = res * hk / 3.0
+	res = res / sqrt(2.0 * cmath.pi)
+	#ofs5 << setprecision(10) << x << " " << norm(res) << " " << real(res) << " " << imag(res) << endl;
+	out = x + " " + str(abs(res)**2) + "\n"
+	f.write(out)
+	x += hx
+
+	k_sum += (tmp1 + norm(res)) * hx / 2.0
+	tmp1 = abs(res)**2
+
+	#cout << "x " << x << endl;
+	#cout << "area " << k_sum << endl;
+    print("x ",str(x))
+    print("area ",str(k_sum))
+
+    tmp1 = abs(f(k1) * ( T1(k1) * cmath.exp(I * k1 * x) + R2(k1) * cmath.exp(-I * k1 * x) ) * cmath.exp( - I * k1 * k1* s) )**2
+    #//入反射2
+    #for(nx = 0; nx <= N3; nx++){
+    for nx in range(0,N3+1):
+	k = k1 + hk
+	res = 0 + 0j
+	#for(jk = 1; jk<ek-1; jk+=2){
+	for jk in range(1,ek-1,2):
+	    res += 4.0 * f(k) * ( T1(k) * cmath.exp(I * k * x) + R2(k) * cmath.exp( -I * k * x ) ) * cmath.exp( - I * k * k * s)
+	    k += hk * 2.0
+	
+	k = k1 + hk + hk
+	#for(jk = 2; jk < ek-1; jk+=2){
+	for jk in range(2,ek-1,2):
+	    res += 2.0 * f(k) * ( T1(k) * cmath.exp(I * k * x) + R2(k) * cmath.exp(-I * k * x) ) * cmath.exp( - I * k * k * s)
+	    k += hk * 2.0
+
+	res += f(k1) * ( T1(k1) * cmath.exp(I * k1 * x) + R2(k1) * cmath.exp(-I * k1 * x )) * cmath.exp( - I * k1 * k1* s)
+	res += f(end_k) * ( T1(end_k)  * cmath.exp(I * end_k * x) + R2(end_k) * cmath.exp(-I * end_k * x)) * cmath.exp( - I * end_k * end_k * s)
+	res  = res * hk / 3.0
+	res = res / sqrt(2.0 * cmath.pi)
+	#ofs5 << setprecision(10) << x << " " << norm(res) << " " << real(res) << " " << imag(res) << endl;
+	out = x + " " + str(abs(res)**2) + "\n"
+	f.write(out)
+	x += hx
+
+	k_sum += (tmp1 + abs(res)**2) * hx / 2.0
+	tmp1 = abs(res)**2
+
+    #cout << "x " << x << endl;
+    #cout << "area " << k_sum << endl;
+    print("x ",str(x))
+    print("area ",str(k_sum))
+
+    k_2 = k2(k1)
+    tmp1 = abs(f(k1) * (A2(k1) * cmath.exp(I * k_2 * x) + B2(k1) * cmath.exp(-I * k_2 * x)) * cmath.exp(-I * k1 * k1* s))**2
+
+    #//障壁2
+    #for(nx = 0; nx <= N4; nx++){
+    for nx in range(0,N4+1):
+	k = k1 + hk
+	res = 0 + 0j 
+	k_2 = k2(k)
+	#for(jk = 1; jk<ek-1; jk+=2){
+	for jk in range(1,ek-1,2):
+	    res += 4.0 * f(k) * (A2(k) * cmath.exp(I * k_2 * x) + B2(k) * cmath.exp(-I * k_2 * x))* cmath.exp(-I * k * k * s)
+	    k += hk * 2.0
+	    k_2 = k2(k)
+
+	k = k1 + hk + hk
+	k_2 = k2(k)
+	#for(jk = 2; jk < ek-1; jk+=2){
+	for jk in range(2,ek-1,2):
+	    res += 2.0 * f(k) * (A2(k) * cmath.exp(I * k_2 * x) + B2(k) * cmath.exp(-I * k_2 * x)) * cmath.exp(-I * k * k * s)
+	    k += hk * 2.0
+	    k_2 = k2(k)
+
+	k_2 = k2(k1)
+	res += f(k1) * (A2(k1) * cmath.exp(I * k_2 * x) + B2(k1) * cmath.exp(-I * k_2 * x)) * cmath.exp(-I * k1 * k1* s)
+	k_2 = k2(end_k)
+	res += f(end_k) * (A2(end_k) * cmath.exp(I * k_2 * x) + B2(end_k) * cmath.exp(-I * k_2 * x)) * cmath.exp(-I * end_k * end_k * s)
+	res  = res * hk / 3.0
+	res = res / sqrt(2.0 * cmath.pi)
+	#ofs5 << setprecision(10) << x << " " << norm(res) << " " << real(res) << " " << imag(res) << endl;
+	out = x + " " + str(abs(res)**2) + "\n"
+	f.write(out)
+	x += hx
+	k_sum += (tmp1 +abs(res)**2) * hx / 2.0
+	tmp1 = abs(res)**2
+
+
+    #cout << "x " << x  << endl;
+    #cout << "area " << k_sum << endl;
+    print("x ",str(x))
+    print("area ",str(k_sum))
+
+    tmp1 = abs(f(k1) * T2(k1) * cmath.exp(I * k1 * x - I * k1 * k1* s))**2
+    #//透過
+    #for(nx = 0; nx <= N5; nx++){
+    for nx in range(0,N5+1):
+	k = k1 + hk
+	res = 0 + 0j 
+	#for(jk = 1; jk<ek-1; jk+=2){
+	for jk in range(1,ek-1,2):
+	    res += 4.0 * f(k) * T2(k) * cmath.exp(I * k * x - I * k * k * s)
+	    k += hk * 2.0
+
+	k = k1 + hk + hk
+	#for(jk = 2; jk < ek-1; jk+=2){
+	for jk in range(2,ek-1,2):
+	    res += 2.0 * f(k) * T2(k) * cmath.exp(I * k * x - I * k * k * s)
+	    k += hk * 2.0
+	
+	res += f(k1) * T2(k1) * cmath.exp(I * k1 * x - I * k1 * k1* s)
+	res += f(end_k) * T2(end_k)  * cmath.exp(I * end_k * x - I * end_k * end_k * s)
+	res  = res * hk / 3.0
+	res = res / sqrt(2.0 * cmath.pi)
+	out = x + " " + str(abs(res)**2) + "\n"
+	f.write(out)
+	x += hx
+	k_sum += (tmp1 +abs(res)**2) * hx / 2.0
+	tmp1 = abs(res)**2
+    
+    #cout << "x " << x  << endl;
+    #cout << "area " << k_sum << endl;
+    print("x ",str(x))
+    print("area ",str(k_sum))
 
 
